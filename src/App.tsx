2de9e2e3ce1e12c564c5929c670622b461dbe725
AngrySquirrel <guigui.maugin@gmail.com>
2023-01-23 16:24:08 +0100

    Form submit fully fonctionnal now

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,28 @@ import BreweryList from "./components/BreweryList";
 import Form from "./components/Form";
 import { formProps, memoryFormat } from "./script/globalInterface";
 import { buttonList, urlPrefix, urlPerPage } from "./script/globalVariable";
-import { by_name, by_city, by_dist_lat, by_dist_lon, by_state, by_postal_code, by_type, by_country } from "./script/globalRef";
-
+import {
+    by_name,
+    by_city,
+    by_dist_lat,
+    by_dist_lon,
+    by_state,
+    by_postal_code,
+    by_type,
+    by_country,
+} from "./script/globalRef";
 
 function App() {
     const [type, setType] = useState<formProps["type"]>("by_name");
     const [memory, setMemory] = useState<memoryFormat[]>([]);
 
     const firstCall = async () => {
-        let response = await fetch(`${urlPrefix}/random?size=${1}`, { // https://api.openbrewerydb.org/breweries/random?size=3
+        let response = await fetch(`${urlPrefix}/random?size=${1}`, {
+            // https://api.openbrewerydb.org/breweries/random?size=3
             mode: "cors",
         });
         let refinedResponse = await response.json();
-        console.log(refinedResponse.length)
+        console.log(refinedResponse.length);
         setMemory([...refinedResponse]);
     };
 
@@ -25,32 +34,34 @@ function App() {
     }, []);
 
     const handleSubmit: React.FormEventHandler<HTMLFormElement> = async (
-      event
-  ) => {
-    console.log("HERE !")
-      let urlSuffix = {
-          by_name: `by_name=${by_name.current?.value
-              ?.replace(" ", "%20")
-              .toLowerCase()}&per_page=${urlPerPage}`,
-          by_city: `by_city=${by_city.current?.value
-              ?.replace(" ", "%20")
-              .toLowerCase()}&per_page=${urlPerPage}`,
-          by_dist: `by_dist=${by_dist_lat.current?.value},${by_dist_lon.current?.value}&per_page=${urlPerPage}`,
-          by_state: `by_state=${by_state.current?.value
-              ?.replace(" ", "%20")
-              .toLowerCase()}&per_page=${urlPerPage}`,
-          by_postal: `by_postal=${by_postal_code.current?.value}&per_page=${urlPerPage}`,
-          by_type: `by_type=${by_type}&per_page${urlPerPage}`,
-          by_country: `by_country=${by_country.current?.value
-              ?.replace(" ", "%20")
-              .toLowerCase()}&per_page=${urlPerPage}`,
-      };
+        event
+    ) => {
+        console.log("HERE !");
+        let urlSuffix = {
+            by_name: `by_name=${by_name.current?.value
+                ?.replace(" ", "%20")
+                .toLowerCase()}&per_page=${urlPerPage}`,
+            by_city: `by_city=${by_city.current?.value
+                ?.replace(" ", "%20")
+                .toLowerCase()}&per_page=${urlPerPage}`,
+            by_dist: `by_dist=${by_dist_lat.current?.value},${by_dist_lon.current?.value}&per_page=${urlPerPage}`,
+            by_state: `by_state=${by_state.current?.value
+                ?.replace(" ", "%20")
+                .toLowerCase()}&per_page=${urlPerPage}`,
+            by_postal: `by_postal=${by_postal_code.current?.value}&per_page=${urlPerPage}`,
+            by_type: `by_type=${by_type}&per_page${urlPerPage}`,
+            by_country: `by_country=${by_country.current?.value
+                ?.replace(" ", "%20")
+                .toLowerCase()}&per_page=${urlPerPage}`,
+        };
 
-      event.preventDefault();
-      let response = await fetch(`${urlPrefix}${urlSuffix}`, { mode: "cors" });
-      let refinedResponse = await response.json();
-      setMemory([...refinedResponse])
-  };
+        event.preventDefault();
+        let response = await fetch(`${urlPrefix}${urlSuffix[type]}`, {
+            mode: "cors",
+        });
+        let refinedResponse = await response.json();
+        setMemory([...refinedResponse]);
+    };
 
     return (
         <div className="App">
@@ -73,7 +84,13 @@ function App() {
             <div className="form">
                 <Form type={type} handleSubmit={handleSubmit}></Form>
             </div>
-            <button onClick={()=>{console.log(memory)}} >Console data</button>
+            <button
+                onClick={() => {
+                    console.log(memory);
+                }}
+            >
+                Console data
+            </button>
             <BreweryList memory={memory} />
         </div>
     );
